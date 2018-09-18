11e633e7a692cebeaf046cefa71db0effc484b30
Jose Miguez <josemiguez184@gmail.com>
2018-09-18 20:29:52 -0300

    Improved token drawing on canvas

--- js/script.js
+++ js/script.js
@@ -12,7 +12,18 @@ function getMousePos(event){
   };
 }
 
-function drawTokensCounter(tokens_number, ctx){
+function reDrawCanvas(mousePos,canvas,token,ctx,board,tokens_number){
+  let newToken = new Circle(mousePos.x,mousePos.y,token.getRadio(),token.getColor());
+  ctx.clearRect(token.getParamX,token.getParamY,30,30);
+  ctx.save();
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.restore();
+  board.draw(ctx);
+  drawTokensCounter(tokens_number,ctx);
+  newToken.draw(ctx);
+}
+
+function drawTokensCounter(tokens_number,ctx){
   ctx.font = "30px Arial";
   ctx.fillStyle = "rgb(255,255,255)";
   ctx.fillText(tokens_number,10,35);
@@ -64,14 +75,13 @@ window.onload = function(){
   canvas.onmousemove = function(e){
     if(drag){
       let mousePos = getMousePos(e);
-      if(aux_token.isClicked(mousePos.x, mousePos.y)){
+      reDrawCanvas(mousePos,canvas,aux_token,ctx,board,tokens_number);
         //Clear the circle and draw another on mouse position
-        let clearToken = new Circle(aux_token.getParamX(), aux_token.getParamY(), aux_token.getRadio(), 'rgba(0,0,0,255)');
-        clearToken.drawWithBorder(ctx,1.8,canvas.style.backgroundColor);
-        let actualToken = new Circle(mousePos.x, mousePos.y, aux_token.getRadio(), aux_token.getColor())
-        actualToken.draw(ctx);
-        aux_token = actualToken;
-      }
+        // let clearToken = new Circle(aux_token.getParamX(), aux_token.getParamY(), aux_token.getRadio(), 'rgba(0,0,0,255)');
+        // clearToken.drawWithBorder(ctx,1.8,canvas.style.backgroundColor);
+         let actualToken = new Circle(mousePos.x, mousePos.y, aux_token.getRadio(), aux_token.getColor())
+        // actualToken.draw(ctx);
+         aux_token = actualToken;
     }
   }
 }
